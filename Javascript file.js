47de095ef8c71efd42c5cfe36af4134115323761
Mufura-Tshipama <st10496960@vcconnect.edu.za>
2025-08-27 11:41:10 +0200

    Add files via upload

---
+++ Javascript file.js
@@ -0,0 +1,7 @@
+<script>
+  function showForm(type) {
+    document.getElementById("orderFormContainer").style.display = "block";
+    document.getElementById("cakeType").value = type;
+  }
+</script>
+    
